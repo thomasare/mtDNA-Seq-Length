2ec69b09cd5546fd142f4798f124618a33609686
thomasare <31910383+thomasare@users.noreply.github.com>
2022-06-20 10:09:52 -0500

    Update dogtest.py

--- dogtest.py
+++ dogtest.py
@@ -55,57 +55,12 @@ def inputData(dataFile, tFile, newName):
 		    else:
 			    file.write(line)
 		
-#get dem files
+#get files
 for s in simFiles:
     fileName = s.split('.')
     inputData(s, 'Template3.xml', 'Beauti_' + str(fileName[1]))
 
-#5. Pull out the incorrect data in data block
-def changeSeq(filetobechanged, newFile):
-    counter = 0
-    badline = '<seqXML xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.seqxml.org/0.4/seqxml.xsd" seqXMLversion="0.4"><entry id='
-    replaceLine = '                <sequence id=' 
-    badline2 = '><description>'
-    badline2_1 = '</description><DNAseq>'
-    replaceLine2_1 = 'totalcount="4" value="'
-    badline3 = '</DNAseq></entry></seqXML><?xml version="1.0" encoding="utf-8"?>'
-    replaceLine3 = '"/>'
-    addline = '<map name="Uniform" >beast.math.distributions.Uniform</map>'
-    addline2 = '</data>'
-    addline3 = '<map name="Uniform" >beast.math.distributions.Uniform</map>'
-    removeline = '<?xml version="1.0" encoding="utf-8"?>'
-    emptyspace = ''
 
-    with open(filetobechanged, 'r') as nr, open(newFile, 'w') as anf:
-        for line in nr.readlines():
-            if badline in line:
-                counter += 1
-                doitbetter = line.replace(badline, replaceLine)
-                if badline2 in doitbetter:
-                    doitbetter2 = doitbetter.replace(badline2, ' taxon="dog' + str(counter) + '" ')
-                    if badline2_1 in doitbetter2:
-                        doitbetter2_1 = doitbetter2.replace(badline2_1, replaceLine2_1)          
-                        if badline3 in doitbetter2_1:
-                            doitbetter3 = doitbetter2_1.replace(badline3, replaceLine3)
-                            anf.write(doitbetter3)
-                                    
-            elif removeline in line:
-                doitbetter4 = removeline.replace(removeline, emptyspace)
-                anf.write(doitbetter4)
-                      
-            elif addline in line:
-                doitbetter5 = addline.replace(addline, addline2)
-                anf.write(doitbetter5)
-                anf.write(addline3)
-                      
-            else:
-                anf.write(line)
-                
-                      
-                
-
-  
-    
 beautiFiles = glob.glob('BEAUti_*.xml')
 count = 0
 for a in beautiFiles:
