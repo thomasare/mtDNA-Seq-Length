8f6ec379c08d7fd73594e653d9d88adf137a0745
thomasare <31910383+thomasare@users.noreply.github.com>
2022-06-20 09:51:28 -0500

    Add files via upload

---
+++ Dog2000_16000_to_1566.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#$ -N Dog2000_16000_to_1566_job
+#$ -e /Users/athomas14/workspace/dna/dissertation/Dog2000_16000_to_1566_job.err
+#$ -o /Users/athomas14/workspace/dna/dissertation/Dog2000_16000_to_1566_job.log 
+#$ -M ariane-thomas@uiowa.edu
+#$ -m abe
+#$ -pe smp 4
+#$ -cwd
+#$ -q ANTH
+
+date start = 'date +%s'
+CMD="
+module load BEAST
+
+java -jar /Users/athomas14/workspace/dna/tools/beast.jar -beagle -beagle_CPU -beagle_SSE /Users/athomas14/workspace/dna/dissertation/Dog2000_16000_to_1566.xml
+
+"
+echo "***************************"
+echo "commands=$CMD"
+echo"****************************"
+
+
+module load BEAST
+java -jar /Users/athomas14/workspace/dna/tools/beast.jar -beagle -beagle_CPU -beagle_SSE /Users/athomas14/workspace/dna/dissertation/Dog2000_16000_to_1566.xml
+
---
+++ Dog2000_2500_to_4500.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#$ -N Dog2000_2500_to_4500_job
+#$ -e /Users/athomas14/workspace/dna/dissertation/Dog2000_2500_to_4500_job.err
+#$ -o /Users/athomas14/workspace/dna/dissertation/Dog2000_2500_to_4500_job.log 
+#$ -M ariane-thomas@uiowa.edu
+#$ -m abe
+#$ -pe smp 4
+#$ -cwd
+#$ -q ANTH
+
+date start = 'date +%s'
+CMD="
+module load BEAST
+
+java -jar /Users/athomas14/workspace/dna/tools/beast.jar -beagle -beagle_CPU -beagle_SSE /Users/athomas14/workspace/dna/dissertation/Dog2000_2500_to_4500.xml
+
+"
+echo "***************************"
+echo "commands=$CMD"
+echo"****************************"
+
+
+module load BEAST
+java -jar /Users/athomas14/workspace/dna/tools/beast.jar -beagle -beagle_CPU -beagle_SSE /Users/athomas14/workspace/dna/dissertation/Dog2000_2500_to_4500.xml
+
